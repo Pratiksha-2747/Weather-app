801fc26144b23b9183e9339016e5cd5970a6934e
Pratiksha <pratikshajani70@gmail.com>
2025-12-15 00:25:49 +0530

    Fetched weather from city name and successfully printing info in console.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let countryOp = document.getElementById('country');
 let stateOp = document.querySelector('#state');
 let cityOp = document.querySelector('#city');
+let submit = document.querySelector('.submit');
 let apikey = "V21hTmtTY25kbTBabncwT2ZPTU5ubTRNTnJDZkdKWXVyc0kxTnNaeg==";
 
 const getStatesByCountry = async (id) => {
@@ -9,7 +10,7 @@ const getStatesByCountry = async (id) => {
     });
 
     const states = await response.json();
-    
+
     for (let s in states) {
         let statecode = states[s].iso2;
         let stateoption = document.createElement("option");
@@ -37,6 +38,7 @@ const getCityByState = async (cid, sid) => {
 }
 
 for (let country in countryCodes) {
+    console.log(country);
     let code = countryCodes[country];
     let option = document.createElement("option");
     option.value = code;
@@ -58,27 +60,85 @@ stateOp.addEventListener("change", () => {
     getCityByState(cid, sid);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log(cityOp.value);
+    weather(cityOp.value);
+});
 
 let apikeyw = "f0ce81830d4ad598f3879e4ad0de54fb";
-
+let weather1 = document.querySelector(".weather");
+let city1 = document.querySelector(".city");
+let data;
 async function weather(city) {
+
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikeyw}`;
     const response = await fetch(url);
-    const data = await response.json();
-    let temp = data.main.temp
-    console.log(temp - 273, "Celc");
-    return data;
+    data = await response.json();
+    let temp = data.main.temp - 273;
+
+    window.open("data.html","_self");
+    // console.log(temp, "Celc");
+    // city1.innerText = `${cityOp.value}`;
+    weather1.innerText = `${temp} Celc`;
 }
+
+// const text = "Weather info app";
+// const textElement = document.querySelector('.h');
+// let index = 0;
+// let isDeleting = false;
+// let typingSpeed = 150;
+// let deletingSpeed = 100;
+// let pauseTime = 2000;
+// textElement.style.fontSize = "30px";
+
+// function type() {
+//     if (!isDeleting && index <= text.length) {
+//         textElement.textContent = text.substring(0, index);
+//         textElement.style.fontSize = "30px";
+//         index++;
+
+//         if (index > text.length) {
+//             setTimeout(() => {
+//                 isDeleting = true;
+//                 type();
+//             }, pauseTime);
+//             return;
+//         }
+
+//         setTimeout(type, typingSpeed);
+//     }
+//     else if (isDeleting && index > 0) {
+//         textElement.textContent = text.substring(0, index);
+//         index--;
+
+//         if (index <= 0) {
+//             setTimeout(() => {
+//                 isDeleting = false;
+//                 index = 0;
+//                 type();
+//             }, 500);
+//             return;
+//         }
+
+//         setTimeout(type, deletingSpeed);
+//     }
+// }
+
+// // Start the animation
+// type();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
